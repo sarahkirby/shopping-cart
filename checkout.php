1588675529cc8cf42543c2d348dedacd4bd5db63
Sarah Kirby <sk.irby@icloud.com>
2016-02-01 13:27:49 +1300

    Add checkout page

---
+++ checkout.php
@@ -0,0 +1,17 @@
+<?php
+
+
+session_start();
+// Include header template
+include 'templates/header.template.php';
+
+// Display contents of the cart
+include 'templates/cart-contents.template.php';
+
+
+
+
+
+
+// Footer
+include 'templates/footer.template.php';
